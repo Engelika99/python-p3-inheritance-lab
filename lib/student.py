a0ae2b5474a66d9a505c2a34caeb4a18c026cf1e
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-07-18 14:18:11 -0400

    Display object inheritance

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+    knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, new_knowledge=""):
+        if type(new_knowledge) == str:
+            self.knowledge.append(new_knowledge)
+        
